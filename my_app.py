865c1b9bcaf009d140b7329e503e43fc89106a89
Zhe Feng <Zhe Feng>
2017-05-28 22:36:38 +0100

    initial commit, hellow world!

---
+++ my_app.py
@@ -0,0 +1,11 @@
+#!project_env/Scripts/python
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, world!", 200
+
+if __name__ == '__main__':
+    app.run()
